7583c142346ab57c807961269401d956969d9319
Abhijit Hiremagalur <abhijit@hiremaga.com>
2012-01-08 20:09:05 -0800

    Remaining exercises for week 1

---
+++ week1/ex3.py
@@ -0,0 +1,22 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6 # Looks like multiple arguments to print are concatenated into a single line.
+print "Roosters", 100.0 - 25 * 3 % 4 # Demonstrates the order of precendence of various operators
+
+print "Now I will count the eggs:"
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7 # Comparison and arithmetic
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# Comparisons
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
